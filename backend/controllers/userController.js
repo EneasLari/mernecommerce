164f7d99c7a59bdadfbac5c0993eee37bed3602e
EneasLari <eneaslari@gmail.com>
2023-05-15 15:56:37 +0300

    episode 44 finished

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,6 @@ import User from '../models/userModel.js'
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
-
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
@@ -28,8 +27,7 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-    console.log("REGISTER")
-    const { name,email, password } = req.body
+    const { name, email, password } = req.body
     const userExists = await User.findOne({ email })
 
     if (userExists) {
@@ -37,8 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User exists')
     }
 
-    const user=await User.create({
-        name,email,password
+    const user = await User.create({
+        name, email, password
     })
 
     if (user) {
